7bc7e76eb73aebb59b4fc866e6298dbbde818c35
Audrey Machivenyika <167446129+audreynm2@users.noreply.github.com>
2025-09-28 22:39:12 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,4 @@
-# arithmetic_operations.py
-
-def perform_operation(num1: float, num2: float, operation: str):
-    """
-    Perform basic arithmetic operations.
-
-    :param num1: first number (float)
-    :param num2: second number (float)
-    :param operation: 'add', 'subtract', 'multiply', or 'divide'
-    :return: result of the operation or an error message
-    """
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
