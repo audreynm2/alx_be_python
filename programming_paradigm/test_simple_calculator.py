4e4394b46e0c82f4ef2e7977dec9489fe9accb08
Audrey Machivenyika <167446129+audreynm2@users.noreply.github.com>
2025-10-12 21:04:20 +0200

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,9 +20,8 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-4, -5), 20)  # edge case
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-6, 3), -2)
-        self.assertEqual(self.calc.divide(5, 2), 2.5)
-        self.assertIs
+        s
